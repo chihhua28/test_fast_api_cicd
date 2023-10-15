31e999cfac2502012304706b2df10137d5b676fc
Chih-Hua Chang <80618804+chihhua28@users.noreply.github.com>
2023-10-15 21:54:14 +0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from typing import Union
+from fastapi import FastAPI
+import requests
+from config import CHATGPT_API_KEY
+
+app = FastAPI()
+
+@app.get("/chatgpt/{prompt}")
+
+def enter_your_question(your_question: Union[str, None] = None):
+    api_key = CHATGPT_API_KEY
+    prompt = your_question
+
+    response = requests.post(
+    'https://api.openai.com/v1/chat/completions',
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}'
+    },
+    json = {
+        'model': 'gpt-3.5-turbo', 
+        'messages' : [{"role": "user", "content": prompt}]
+    })
+
+    json = response.json()
+    return(json)        
