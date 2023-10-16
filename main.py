ba78b8690ca2b4b7e45ea47ffe22d6002794064b
Fiona <fionachang@OneADdeMacBook-Pro-4.local>
2023-10-16 15:28:37 +0800

    add cocktail api

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Union
 from fastapi import FastAPI
 import requests
 import os
+import json
 
 app = FastAPI()
 
@@ -23,4 +24,37 @@ def enter_your_question(api_key, question: Union[str, None] = None):
     })
 
     json = response.json()
-    return(json)        
+    return(json)     
+
+@app.get("/cocktails/{cocktail_name}")
+def get_cocktail_by_name(cocktail_name: str):
+    url = f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={cocktail_name}"
+    response = requests.get(url)
+    data = response.json()
+    cocktails_data = []
+    for drink in data['drinks']:    
+        cocktail_info = {
+            'strDrink': drink['strDrink'],
+            'strInstructions': drink['strInstructions']
+        }
+        cocktails_data.append(cocktail_info)
+
+    with open('cocktails.json', 'w') as json_file:
+        json.dump(cocktails_data, json_file, indent=4)
+
+    with open('cocktails.json', 'r') as json_file:
+        cocktails_json = json.load(json_file)
+
+    return(cocktails_json)
+
+
+
+
+
+
+
+
+
+
+
+ 
--- test_main.py
+++ test_main.py
@@ -2,6 +2,7 @@ import pytest
 from fastapi.testclient import TestClient
 from main import app
 import os
+import request
 
 client = TestClient(app)
 
@@ -16,3 +17,18 @@ def test_enter_your_question():
 
     # message = response.json()["message"]
     # assert len(message) < 40
+
+def test_get_cocktail_by_name():
+
+    response = client.get("/cocktails/long island")
+    data = response.json()
+
+
+    # Check if the response status code is 200
+    assert response.status_code == 200
+
+    # Check if the response contains the expected keys
+    expected_keys = ["strDrink", "strInstructions"]
+
+    for cocktail in data["drinks"]:
+        assert all(key in cocktail for key in expected_keys), f"Response does not contain the expected keys for cocktail: {cocktail['strDrink']}"
