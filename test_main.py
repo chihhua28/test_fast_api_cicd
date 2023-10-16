97430566f198c503e6431a31ebd96876afe9b84d
Fiona <fionachang@OneADdeMacBook-Pro-4.local>
2023-10-16 14:31:32 +0800

    remove expected key restriction

--- test_main.py
+++ test_main.py
@@ -10,14 +10,9 @@ def test_enter_your_question():
     api_key = os.environ.get('CHATGPT_API_KEY')
     question = "How are you?"
     response = client.get(f"/chatgpt/{question}", params={"api_key": api_key})
-    # response = client.get(f"/chatgpt/{question}?api_key={api_key}")
 
     # Check if the response status code is 200
     assert response.status_code == 200
-
-    # Check if the response contains the expected keys
-    expected_keys = ["model", "messages"]
-    assert all(key in response.json() for key in expected_keys)
 
     message = response.json()["message"]
     assert len(message) < 40
