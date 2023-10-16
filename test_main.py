fb0e244a251c048d0900fc60e3edca8dcfc25f05
Fiona <fionachang@OneADdeMacBook-Pro-4.local>
2023-10-16 14:28:42 +0800

    edit response

--- test_main.py
+++ test_main.py
@@ -9,7 +9,8 @@ def test_enter_your_question():
 
     api_key = os.environ.get('CHATGPT_API_KEY')
     question = "How are you?"
-    response = client.get(f"/chatgpt/{question}?api_key={api_key}")
+    response = client.get(f"/chatgpt/{question}", params={"api_key": api_key})
+    # response = client.get(f"/chatgpt/{question}?api_key={api_key}")
 
     # Check if the response status code is 200
     assert response.status_code == 200
