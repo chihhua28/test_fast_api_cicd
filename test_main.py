51fbaef8c2332e362a2466e2ecdd3abcf7af1a6d
Fiona <fionachang@OneADdeMacBook-Pro-4.local>
2023-10-16 14:35:05 +0800

    remove message length restriction

--- test_main.py
+++ test_main.py
@@ -14,5 +14,5 @@ def test_enter_your_question():
     # Check if the response status code is 200
     assert response.status_code == 200
 
-    message = response.json()["message"]
-    assert len(message) < 40
+    # message = response.json()["message"]
+    # assert len(message) < 40
